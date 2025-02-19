cb5fd774a175a58a18da3cbb8c0d50bc3aa70cbc
Montavius Spratley Burford <montavius.cs@yahoo.com>
2025-02-18 23:25:20 -0500

    CHP2_20_HMP.cpp

--- CHP2_20_HMP.cpp
+++ CHP2_20_HMP.cpp
@@ -1,25 +1,26 @@
 /*
-Program: How much paint
+Program: How much paint 2
 Programmer: Montavius Spratley Burford
 Date: 2/9/2025
-Requirments: Write a program to determine and report approximately how much pain will be 
-needed to paint two coats on a wooden fence that is 6 feet high, 100 feet long, and 100 feet wide.
+Requirments: Use your previous program from Exercise 20 and 
+revise it so that the user can enter the size of the fence.
+The revised program should be pushed 
+to a new branch of the project from Exercise 20
 */
 
 #include <iostream>
-#include<cmath>
+#include <cmath>
 
-int main()
-{
-    const double fenceHeight = 6.0; 
-    const double fenceLength = 100.0;
+int main() {
+    double fenceHeight, fenceLength;
+    const double coveragePerGallon = 340.0; // square feet per gallon
 
-    
-    const double coveragePerGallon = 340.0; 
+    std::cout << "Enter the height of the fence (in feet): ";
+    std::cin >> fenceHeight;
+    std::cout << "Enter the length of the fence (in feet): ";
+    std::cin >> fenceLength;
 
-    
-    double totalArea = 2 * fenceHeight * fenceLength * 2; 
-
+    double totalArea = 2 * fenceHeight * fenceLength * 2; // 2 coats
     int gallonsNeeded = std::ceil(totalArea / coveragePerGallon);
 
     std::cout << "You will need approximately " << gallonsNeeded << " gallons of paint." << std::endl;
